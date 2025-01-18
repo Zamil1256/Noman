604c9557543dd578c0ae431d8311c440d75d7e15
Zamil1256 <zamilhowleder3242@gmail.com>
2025-01-18 10:28:29 +0600

    Delete nom.py

--- nom.py
+++
@@ -1,9 +0,0 @@
-#Looping
-cities=("Barishal","Dhaka","Khulna")
-#print(cities[0])
-#print(cities[1])
-#print(cities[2])
-#for city in cities:
-#    print(city)
-for i in range(len(cities)):
-    print(cities[i])
